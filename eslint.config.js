bb687a2237a469fd5e1972a0012b2472a690cabf
juanbig <juannodecoorp@gmail.com>
2024-09-22 10:12:56 -0400

    CI work 2-part ++++

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    env: {
-      node: true,
-      browser: true,
-    },
     extends: 'eslint:recommended',
     parserOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+    },
+    languageOptions: {
+      globals: {
+        node: true,
+        browser: true,
+      },
     },
     rules: {
       'no-console': 'off',
